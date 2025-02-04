fcdc13d81c3f10a5de89d384d8d7616a4657aa9a
Amir Ali V <amiralivillan@gmail.com>
2025-02-04 22:18:46 +0530

    lrrt

---
+++ string/FirstUniqueCharacterString.java
@@ -0,0 +1,17 @@
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charCount.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

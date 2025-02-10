3795339da67a3eb8a4db01253660a32f06d379e6
Amir Ali V <amiralivillan@gmail.com>
2025-02-10 09:52:31 +0530

    leet

---
+++ arrays/FisrtPallindromicString.java
@@ -0,0 +1,26 @@
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// A string is palindromic if it reads the same forward and backward.
+
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String s : words) {
+            if (isPalindrome(s)) {
+                return s;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
